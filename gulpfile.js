ec779542b6213d89b56779534f2753dc56f26cb3
NurulSuhada <82923355+NurulSuhada@users.noreply.github.com>
2021-12-24 13:06:55 +0800

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+const { series, parallel, src, dest, watch } = require('gulp')
+const { sync } = require('glob')
+const gulpSass = require('gulp-sass')
+const nodeSass = require('node-sass')
+const sass = gulpSass(nodeSass);
+//const sass = require('gulp-sass')
+const { join, basename } = require('path')
+const cleanCSS = require('gulp-clean-css')
+const rename = require('gulp-rename')
+const browserSync = require('browser-sync').create()
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const sourcemaps = require('gulp-sourcemaps')
+const { exec } = require('child_process')
+const { promisify } = require('util')
+require('colors')
+//sass.compiler = require('node-sass')
+const path = join(__dirname, 'src')
+const execAsync = promisify(exec)
+
+const compileSCSS = () =>
+  src(sync(join(path, 'scss', '**/*.scss')))
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(join(path, 'dist')))
+
+const compileJS = () =>
+  src(sync(join(path, 'js', '**/*.js')))
+    .pipe(sourcemaps.init())
+    .pipe(
+      babel({
+        presets: ['@babel/env'],
+      })
+    )
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(join(path, 'dist')))
+
+const minifyCSS = () =>
+  src(sync(join(path, 'dist', '**/!(*.min).css')))
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(
+      rename(({ dirname, basename }) => ({
+        dirname,
+        basename: `${basename}.min`,
+        extname: '.css',
+      }))
+    )
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(join(path, 'dist')))
+
+const minifyJS = () =>
+  src(sync(join(path, 'dist', '**/*.js')))
+    .pipe(uglify())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(join(path, 'dist')))
+
+const dev = () => {
+  browserSync.init({
+    server: {
+      baseDir: join(path),
+    },
+  })
+}
+
+const watchJsAndSCSS = (cb) => {
+  const jsFiles = sync(join(path, 'js', '**/*.js'))
+  console.log(`👁️ ${'JavaScript'.yellow} files we will watch... 👁️`.bold)
+  console.table(jsFiles.map((path) => basename(path)))
+  watch(jsFiles, series(compileJS, minifyJS, realoadBrowser))
+  const scssFiles = sync(join(path, 'scss', '**/*.scss'))
+  console.log(`👁️ ${'SCSS'.magenta} files we will watch... 👁️`.bold)
+  console.table(scssFiles.map((path) => basename(path)))
+  watch(scssFiles, series(compileSCSS, minifyCSS, realoadBrowser))
+  cb()
+}
+
+const realoadBrowser = (cb) => {
+  browserSync.reload()
+  cb()
+}
+
+const execShellStuff = async () => await execAsync
+
+exports.default = series(
+  parallel(compileJS, compileSCSS),
+  parallel(minifyCSS, minifyJS),
+  watchJsAndSCSS,
+  execShellStuff,
+  dev
+)
---
+++ src/dist/main.js
@@ -0,0 +1,2 @@
+"use strict";var getData=function(){return fetch("https://jsonplaceholder.typicode.com/posts").then(function(t){return t.json()}).then(console.log)},data=[1,2,3,4,5,6,7],one=data[0],two=data[1],three=data[2],data2=[].concat(data,[[8,9,10]]);
+//# sourceMappingURL=main.js.map
